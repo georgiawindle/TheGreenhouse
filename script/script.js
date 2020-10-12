20f557c86cc192c1243e15142b0be2152b9b2caf
blaineanthonywestern <blaine.a.western@gmail.com>
2020-10-12 17:14:52 +1300

    add changes to nav

--- script/script.js
+++ script/script.js
@@ -1,6 +1,16 @@
-const toggleButton = document.getElementsByClassName ('toggle-button')[0]
-const navbarlinks = document.getElementsByClassName ('navbar-links')[0]
+const navbarlinks = document.getElementsByClassName("navbar-links");
 
-toggleButton .addEventListener('click', () => {
-    navbarlinks.classList.toggle('active')
-})
+function toggleNav() {
+  if (navbarlinks[0].classList.contains("active")) {
+    navbarlinks[0].classList.remove("active");
+  } else {
+    navbarlinks[0].classList.add("active");
+  }
+}
+
+// window.onload = function() {
+//   toggleButton.addEventListener("click", () => {
+//     navbarlinks.classList.toggle("active");
+//     console.log("Clicked!");
+//   });
+// };
